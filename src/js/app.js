208b94177289ec7bfdf6b484c760b89993c23de6
Kylik Dmitriy <BkForest20@gmail.com>
2019-03-28 09:57:12 +0200

    Added responsive styles

--- src/js/app.js
+++ src/js/app.js
@@ -40,6 +40,22 @@ $(function() {
     rows: 1,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
   /*----------TEAM SLIDER END--------------*/
 });
